c12bd83e255ac19d1b6a97a46b777d3cdb8fb866
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2024-08-03 20:40:30 +0530

    get inventories endpoint

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schema";
+import { inventories, products, warehouses } from "@/lib/db/schema";
 import { inventorySchema } from "@/lib/validators/inventoriesSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request:Request) {
      const requestData = await request.json();
@@ -18,4 +19,22 @@ export async function POST(request:Request) {
      } catch (error) {
         return Response.json({ message: "Failed to store the inventories" }, { status: 500 });
      }
+}
+
+
+export async function GET(){
+   try {
+      const inventoriesData = await db.select({
+         id:inventories.id,
+         sku:inventories.sku,
+         warehouses:warehouses.name,
+         products:products.name,
+      }).from(inventories)
+      .leftJoin(warehouses,eq(inventories.warehouseId,warehouses.id))
+      .leftJoin(products,eq(inventories.productId,products.id))
+      .orderBy(desc(inventories.id));
+      return Response.json(inventoriesData)
+   } catch (error) {
+      return Response.json({ message: "Failed to fetch the inventories data" }, { status: 500 });
+   }
 }
