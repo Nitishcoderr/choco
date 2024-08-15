e2082098b2ee637d7e8f9b940c6d7a7186b04d76
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2024-08-15 17:09:32 +0530

    add skeleton single product page

--- src/http/api.ts
+++ src/http/api.ts
@@ -71,3 +71,14 @@ export const getAllInventories = async()=>{
   const response = await api.post('/inventories',data);
   return response.data;
 }
+
+
+export const getSingleProduct = async(id:string)=>{
+  try {
+    const response =  await api.get(`/products/${id}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+   console.log(error);
+  }
+ }
