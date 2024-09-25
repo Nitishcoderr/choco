84df9ffd1833497ab1b83f7be8b9693d5b832c97
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2024-09-25 22:46:46 +0530

    "Added authentication logic to Header component, including sign out button and sign in link"

--- src/app/(client)/_components/header.tsx
+++ src/app/(client)/_components/header.tsx
@@ -1,40 +1,48 @@
 'use client';
-
 import { cn } from '@/lib/utils';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Header() {
-    const pathname = usePathname();
+  const pathname = usePathname();
+  const session = useSession();
 
-    const navItems = [
-        { label: 'Home', href: '/' },
-        { label: 'Best Selling', href: '/best-selling' },
-        { label: 'Offers', href: '/offers' },
-        { label: 'Orders', href: '/account/orders' },
-    ];
+  const navItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Best Selling', href: '/best-selling' },
+    { label: 'Offers', href: '/offers' },
+    { label: 'Orders', href: '/account/orders' },
+  ];
 
-    return (
-        <header className="">
-            <div className="flex h-10 items-center justify-center bg-brown-900 text-center text-white">
-                <span className="text-sm">
-                    Order 2 Delight Dairy Choco bars today and save ₹100 instantly!
-                </span>
-            </div>
-            <nav className="flex h-14 items-center justify-center">
-                <ul className="flex items-center justify-center gap-6">
-                    {navItems.map((item) => (
-                        <li
-                            key={item.href}
-                            className={cn(
-                                'text-brown-300 underline-offset-4 transition-all hover:cursor-pointer hover:text-brown-900 hover:underline',
-                                pathname === item.href && 'font-semibold text-brown-900 underline'
-                            )}>
-                            <Link href={item.href}>{item.label}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="">
+      <div className="flex h-10 items-center justify-center bg-brown-900 text-center text-white">
+        <span className="text-sm">
+          Order 2 Delight Dairy Choco bars today and save ₹100 instantly!
+        </span>
+      </div>
+      <nav className="flex h-14 items-center justify-around">
+        <ul className="flex items-center justify-center gap-6 md:w-[99%]">
+          {navItems.map((item) => (
+            <li
+              key={item.href}
+              className={cn(
+                'text-brown-300 underline-offset-4 transition-all hover:cursor-pointer hover:text-brown-900 hover:underline',
+                pathname === item.href && 'font-semibold text-brown-900 underline'
+              )}>
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+          <div className="text-white underline-offset-4 transition-all hover:cursor-pointer md:w-[1%] flex justify-end md:mr-16 ">
+            {session.status === 'authenticated' ? (
+              <button className='bg-black/70 hover:bg-black/60 rounded-md px-2 py-1' onClick={()=>signOut()} >Logout</button>
+            ) : (
+              <Link href="/api/auth/signin">Sign IN</Link>
+            )}
+          </div>
+      </nav>
+    </header>
+  );
 }
